7a0edc302106731f578b2fa2e389f80e6bfc5ad9
Karen Mazidi <kjmazidi@users.noreply.github.com>
2020-02-04 11:35:36 -0600

    Update prob7.asm

--- Code Samples/Section 2/practice_problems_solutions/prob7.asm
+++ Code Samples/Section 2/practice_problems_solutions/prob7.asm
@@ -7,15 +7,16 @@ s1:	.asciiz	"hi"
 s2:	.space	4
 
 	.text
-main:	la	$t1, s1
-	la	$t2, s2
+main:	la	$a1, s1
+	la	$a2, s2
 	jal	copy
 
 exit:	li	$v0, 10
 	syscall
 	
 copy:
-
+	add	$t1, $a1, $0	# $t1 = s1[]
+	add	$t2, $a2, $0	# $t2 = s2[]
 	lbu	$t5, ($t1)	# $t5 = s1[i]
 	sb	$t5, ($t2)	# s2[i] = $t5
 	beq	$t5, $zero, return
