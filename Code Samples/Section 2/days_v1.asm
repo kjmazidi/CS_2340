36fc9df5de0340b9f674bf883f9f0e9b70654104
Karen Mazidi <kjmazidi@users.noreply.github.com>
2020-02-05 11:51:20 -0600

    Update days_v1.asm

--- Code Samples/Section 2/days_v1.asm
+++ Code Samples/Section 2/days_v1.asm
@@ -1,10 +1,14 @@
-# hackathon program 1, version 1
+# days_v1.asm
+# Karen Mazidi
+# Program to output number of days for a given month
+#  program does not handle leap years
 
 	.data
 msg1:	.asciiz	"Number of days in month "
 msg2:   .asciiz " is: "
 msgerr: .asciiz "Month must be between 1 and 12\n"
 msgent: .asciiz "Please enter the month 1 - 12, enter 0 to quit: "
+
 	.text	
 loop:	# get month from user
 	la	$a0, msgent
@@ -13,32 +17,39 @@ loop:	# get month from user
 	li	$v0, 5
 	syscall
 	move	$t0, $v0
-	# check for valid month
-	beq	$t0, $zero, exit
+	
+	# check for valid month, 0 to quit
+	beq	$t0, $zero, exit  
 	blt	$t0, 1, err
 	bgt	$t0, 12, err
-	# print msg1
+	
+	# print "Number of days in month:"
 	la	$a0, msg1
 	li	$v0, 4
 	syscall
+	
 	# print month
 	move	$a0, $t0
 	li	$v0, 1
 	syscall
-	# print msg2
+	
+	# print " is: "
 	la	$a0, msg2
 	li	$v0, 4
 	syscall
+	
 	# print number of days
 	move	$a0, $t0
 	jal	days
 	move	$a0, $v0
 	li	$v0, 1
 	syscall
-	# print return
+	
+	# print line return
 	li	$a0, 0x0a
 	li	$v0, 11
 	syscall
+	
 	# repeat
 	j	loop
 	
@@ -47,10 +58,12 @@ err:	# print error message
 	li	$v0, 4
 	syscall
 	j	loop	
+	
 exit:	li	$v0, 10
 	syscall	
-	
-days:	# return number of days
+###################################################	
+days:	# return number of days in $v0
+	# month is in $a0 (1 - 12)
 	li	$v0, 30		# default case
 	beq	$a0, 4, ret
 	beq	$a0, 6, ret
